6e75460fdab2ee04e8854f0eb7fb96db45c23c03
Маис <Kozannostra777@mail.ru>
2022-07-22 22:34:23 +0300

    7

---
+++ Logo.java
@@ -0,0 +1,30 @@
+import java.util.logging.*;
+public class Logo {
+
+   public static void main(String[] args) {
+
+       Logger logger = Logger.getLogger(Logo.class.getName());
+
+       logger.setLevel(Level.INFO);
+
+       ConsoleHandler ch = new ConsoleHandler();
+
+       logger.addHandler(ch);
+
+       //SimpleFormatter sFormat = new SimpleFormatter();
+
+       XMLFormatter xml = new XMLFormatter();
+
+       //ch.setFormatter(sFormat);
+
+       ch.setFormatter(xml);
+
+       logger.log(Level.WARNING, "Тестовое логирование");
+
+       logger.info("Тестовое логирование");
+
+   }
+
+}
+
+    
