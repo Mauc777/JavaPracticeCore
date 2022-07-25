33ba345a6864aff3c617c49c4bb08eac7f5edde0
Маис <Kozannostra777@mail.ru>
2022-07-25 14:13:41 +0300

    125252

--- CollectionJavaCore/DequeCollection.java
+++ CollectionJavaCore/DequeCollection.java
@@ -6,7 +6,6 @@ import java.util.Deque;
 public class DequeCollection {
     public static void main(String[] args) {
         Deque<Integer> dq = new ArrayDeque<>();
-        // Deque<Integer> dq = new ArrayDeque<>();
         dq.addFirst(1);
         dq.addLast(2);
         dq.removeLast();
